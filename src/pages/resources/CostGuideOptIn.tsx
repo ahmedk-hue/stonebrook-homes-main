28ab3c4426433dabeec262e9d26fdcee356f7a69
Ahmed <abc@MacBook-Air.local>
2026-02-10 18:22:03 +0200

    Further adjust pricing page layout - increase vertical padding

--- src/pages/resources/CostGuideOptIn.tsx
+++ src/pages/resources/CostGuideOptIn.tsx
@@ -45,7 +45,7 @@ const CostGuideOptIn = () => {
             </Helmet>
 
             <div className="min-h-screen bg-stone-50">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 lg:py-24">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24 lg:py-32">
                     <div className="grid lg:grid-cols-2 gap-12 lg:gap-20 items-start">
 
                         {/* Left Column: Copy */}
